2bdd44e9d23d6a5e53044fdc8a7a4ffcfa09dd12
Joel Eze <jl_eze@yahoo.co.uk>
2014-08-21 17:28:01 +0100

    Layout In Android

---
+++ src/com/layout/example/Layout.java
@@ -0,0 +1,15 @@
+package com.layout.example;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Layout extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.layout);
+	}
+	
+}
